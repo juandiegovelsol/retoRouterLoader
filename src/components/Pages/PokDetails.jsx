96fecb1de4f4a5d8fde0d662a6534fd270ae0a26
Juan <juandiegovelsol@gmail.com>
2023-03-06 22:17:12 -0500

    pokemon details page updated with recharging page feature

--- src/components/Pages/PokDetails.jsx
+++ src/components/Pages/PokDetails.jsx
@@ -11,9 +11,24 @@ const PokDetails = () => {
   const { characters } = pokemon || {};
   const { name, order, height, image } = character || {};
 
+  const getOnePokemon = async (id) => {
+    let pokemon = {};
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const image = data.sprites.front_default;
+    const { name, order, height } = data;
+    pokemon = { name, id, image, order, height };
+    setCharacter(pokemon);
+  };
+
   useEffect(() => {
     const item = characters.find((item) => item.id.toString() === idParam);
-    setCharacter(item);
+    if (item) {
+      setCharacter(item);
+    } else {
+      getOnePokemon(idParam);
+    }
   }, []);
 
   return <PokDetail name={name} order={order} height={height} image={image} />;
